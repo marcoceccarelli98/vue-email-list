b9d4691e7bbe9d9e4f47664294b4eec01dbbeb43
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-29 10:47:11 +0200

    [UPD] Converted main.js to easier code

--- main.js
+++ main.js
@@ -6,38 +6,27 @@ createApp({
   data() {
     return {
       url: "https://flynn.boolean.careers/exercises/api/random/mail",
-      emailList: [],
+      emails: [],
       num: 10,
     };
   },
 
   methods: {
-    async apiCall() {
-      try {
-        const response = await axios.get(this.url);
-        return response.data.response;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        // Handle the error as needed
-        return null; // or throw the error, or any other error handling logic
-      }
-      //axios.get(this.url).then((response) => {
-      //return response.data.response;
-      //console.log(response.data.response);
+    apiCall() {
+      return axios
+        .get(this.url)
+        .then((response) => this.emails.push(response.data.response))
+        .catch((error) => console.log(error));
     },
 
-    async getEmails(num) {
+    getEmails(num) {
       for (let i = 0; i < num; i++) {
-        const email = await this.apiCall();
-        if (email) {
-          this.emailList.push(email);
-        }
+        this.apiCall();
       }
     },
   },
 
-  async created() {
-    await this.getEmails(this.num);
-    console.log(this.emailList);
+  created() {
+    this.getEmails(this.num);
   },
 }).mount("#app");
